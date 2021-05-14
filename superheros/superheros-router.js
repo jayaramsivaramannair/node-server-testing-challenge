f9d2bed13806323994c2b394d4c96ac5d0c28da4
jayaramsivaramannair <jayaramsivaramannair@gmail.com>
2021-05-13 21:40:59 -0400

    Wrote two tests each for delete and post endpoints

--- superheros/superheros-router.js
+++ superheros/superheros-router.js
@@ -11,4 +11,32 @@ router.get("/", async (req, res, next) => {
     }
 })
 
+router.post("/", async (req, res, next) => {
+    try {
+        const newHero = await SuperHeros.create({
+            name: req.body.name,
+            superpower: req.body.superpower,
+        })
+        res.status(201).json(newHero)
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.delete("/:id", async (req, res, next) => {
+    try {
+        const hero = await SuperHeros.findById(req.params.id)
+        if (!hero) {
+            return res.status(404).json({
+                message: "Hero not found",
+            })
+        } else {
+            const fallenHero = await SuperHeros.remove(req.params.id)
+            res.status(200).json(fallenHero)
+        }
+    } catch (err) {
+        next(err)
+    }
+})
+
 module.exports = router
